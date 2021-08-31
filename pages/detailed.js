05ec3e0b250384738ef15365d2003637c049a757
kent99821 <2270514313@qq.com>
2021-08-31 20:04:20 +0800

    完成项目布局

---
+++ pages/detailed.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Row, Col} from 'antd'
+import  Head  from "next/head";
+import Header from "../components/Header";
+export default function detailed() {
+  return (
+    
+  <div>
+    <Head>
+      <title>detailed</title>
+    </Head>
+    <Header/>
+    <Row className="comm-main" type="flex" justify="center">
+      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14} xxl={10}>
+        left
+      </Col>
+      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4} xxl={3}>
+      right
+      </Col>
+    </Row>
+  </div>
+    )
+    
+}
